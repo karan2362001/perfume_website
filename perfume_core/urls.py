277b40e4a75499ee56c7c36f929f0a2979e42d08
anshp2003 <anshpatel20003ap@gmail.com>
2024-08-02 14:00:24 +0530

    error check

--- perfume_core/urls.py
+++ perfume_core/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 
 
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # Serve media files in development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
